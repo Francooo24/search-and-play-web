1a5c48ad158c94198f8159d077dd5ced5d22c033
Franco Medina <franco02medina@gmail.com>
2026-04-03 20:55:32 +0800

    describe your changes

--- components/SessionProvider.tsx
+++ components/SessionProvider.tsx
@@ -2,5 +2,5 @@
 import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
 
 export default function SessionProvider({ children }: { children: React.ReactNode }) {
-  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
+  return <NextAuthSessionProvider refetchOnWindowFocus={false} refetchInterval={0}>{children}</NextAuthSessionProvider>;
 }
