f1df77372fff7d2e02a89e90c35822638b7ba72c
Franco Medina <franco02medina@gmail.com>
2026-04-11 14:07:44 +0800

    updated

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -318,7 +318,7 @@ export default function SignupPage() {
           </div>
 
           {/* Email - row 4 left */}
-          <div className="relative">
+          <div className="relative self-start">
             <div className="absolute left-4 top-1/2 -translate-y-1/2 text-orange-400 opacity-70 pointer-events-none">
               <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>
             </div>
